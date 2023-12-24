f4482a7ac3ac46205f8bafb8eae285ee3b9f0a1d
kazmabbas <105518597+kazmabbas@users.noreply.github.com>
2023-12-24 14:19:00 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -7,9 +7,9 @@ class Config(object):
     STRING_SESSION = os.environ.get("STRING_SESSION", "")
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-    SUPPORT = os.environ.get("SUPPORT", "HunerThon")
-    CHANNEL = os.environ.get("CHANNEL", "HunerThon")
-    START_IMG = os.environ.get("START_IMG", "https://graph.org/file/673bd8e51c4fef00c2848.jpg")
-    CMD_IMG = os.environ.get("CMD_IMG", "https://graph.org/file/673bd8e51c4fef00c2848.jpg")
+    SUPPORT = os.environ.get("SUPPORT", "Repethone")
+    CHANNEL = os.environ.get("CHANNEL", "Repethone")
+    START_IMG = os.environ.get("START_IMG", "https://graph.org/file/74485f30f6b76b038146e.jpg")
+    CMD_IMG = os.environ.get("CMD_IMG", "https://graph.org/file/74485f30f6b76b038146e.jpg")
     OWNER_ID = os.environ.get("OWNER_ID", "")
 
